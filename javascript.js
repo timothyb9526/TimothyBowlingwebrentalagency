22da9710e8795aee8967491263a0163b446f7e28
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-10-19 15:22:36 -0500

    added an end page and updated the pages

--- javascript.js
+++ javascript.js
@@ -1,21 +1,27 @@
-var element = document.getElementById("cabin-info");
-var rental = document.getElementById("hide");
-var message = document.getElementById("show");
+var cabinInfo = document.getElementById("cabin-info");
+var rental = document.getElementById("rental");
+var message = document.getElementById("message");
 
 function rent() {
     var source = document.getElementById("rentals").innerHTML;
     var template = Handlebars.compile(source);
     var result = "";
-    for (var cabin of RENTAL_DATA.inventory.cabins) {
+    for (var cabin in RENTAL_DATA.cabins) {
+        var obj = RENTAL_DATA.cabins[cabin];
+        var image = obj.coverImage;
+        var price = obj.price;
+        var description = obj.description;
+        var location = obj.location;
         var html = template({
-            picture: cabin.coverImage,
-            price: cabin.price,
-            description: cabin.description,
-            location: cabin.location
+            picture: image,
+            price: price,
+            description: description,
+            location: location
+            // index: i
         });
         result += html;
     }
-    element.insertAdjacentHTML("beforeend", result);
+    cabinInfo.insertAdjacentHTML("beforeend", result);
 }
 // rent();
 
@@ -23,29 +29,46 @@ var form = document.getElementById("form");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    rental.style.display = "none";
-    message.style.fontSize = "5rem";
-    message.style.color = "black";
-    message.style.fontWeight = "bolder";
+    rentItem();
 });
 
 function rentalButtonLoad() {
-    var button = document.getElementsByClassName("rental");
+    // var button = document.getElementsByClassName("rental");
     var rentalButton = document.querySelectorAll(".rental");
     console.log(rentalButton);
-    for (var i of rentalButton) {
-        i.addEventListener("click", function() {
+    for (var i in rentalButton) {
+        var button = rentalButton[i];
+        let index = i;
+        button.addEventListener("click", function() {
+            var input = document.querySelector("#form-index");
+            input.value = index;
             form.style.display = "block";
-            for (var b of button) {
-                b.style.display = "none";
-            }
+            cabinInfo.style.display = "none";
         });
     }
+}
+
+function rentItem() {
+    var input = document.querySelector("#form-index");
+    var index = input.value;
+    console.warn(index);
+    var item = RENTAL_DATA.cabins[index];
+    var price = item.price;
+    console.log(item);
+    var priceSpan = document.querySelector("#price");
+    priceSpan.innerHTML += price;
+    rental.style.display = "none";
+    message.classList.add("special-class");
+}
+
+function reset() {
+    document.querySelector("form").reset();
 }
 
 function startUp() {
     rent();
     rentalButtonLoad();
+    reset();
 }
 
 window.addEventListener("load", startUp);
--- rental_data.js
+++ rental_data.js
@@ -1,53 +1,52 @@
 var RENTAL_DATA = {
-    inventory: {
-        cabins: [
-            {
-                coverImage: "pictures/georgia-cabin.jpg",
-                price: 101,
-                description:
-                    "This dog-friendly cabin is perfect for enjoying the Georgia wilderness with your family and friends, with amenities like a private hot tub and pool table alongside the natural beauty around you!Six miles from the Bavarian village of Helen, this Sautee Nacoochee home for eight straddles the middle-ground of remote natural wonder and accessibility to nearby home comforts - in the case of the hot tub on the lower deck, they're right at your home! \
+    cabins: [
+        {
+            coverImage: "pictures/georgia-cabin.jpg",
+            price: 101,
+            description:
+                "This cabin is perfect for enjoying the Georgia wilderness with your family and friends, with amenities like a private hot tub and pool table alongside the natural beauty around you!Six miles from the Bavarian village of Helen, this Sautee Nacoochee home for eight straddles the middle-ground of remote natural wonder and accessibility to nearby home comforts - in the case of the hot tub on the lower deck, they're right at your home! \
                 Sit back and relax in the living room, with easy access to the hot tub and pool table of the loft, and lots of comfy furnishings for relaxing with your friends and family. Start up the fireplace, use the WiFi to plan something new, or enjoy movies on the flatscreen TVs DVD player! \
                 The kitchens stainless steel appliances - combined with the gas grill and firepit outdoors - make whipping up delicious meals a breeze. Eat indoors or take your meals in on the deck with some fresh air! \
                 Eight guests can use this homes two bedrooms and baths, alongside three dogs for a small nightly fee. Each room boasts luxurious king-size beds and private TVs, with the master bedroom offering access to the upper level deck. Two twin rollaway beds and a futon round out the sleeping options. Between the two baths and private washer/dryer, youll be nice and refreshed when you head out to explore! \
                 The nearby village of Helen has lots to do, from touring nearby wineries, visiting local restaurants, or simply enjoying the Bavarian charm of the town. You also have access to lots of natural wonders, like Unicoi State Park and hikes like Dukes Creek waterfall. \
                 This Sautee Nacoochee home provides the woodland getaway you need - place your reservation today!",
-                location: "Helen, GA, USA"
-            },
-            {
-                coverImage: "pictures/gatlinburg-cabin.jpg",
-                price: 150,
-                description:
-                    "Get away from it all and spend some quality time in the Great Smoky Mountains! This 2-bedroom, 2.5-bathroom vacation rental cabin in Gatlinburg comfortably sleeps 6 guests who are looking for a quiet mountain holiday. The 2,500-square-foot home features a beautiful porch that looks out to the woods, as well as a private hot tub with room for everyone in your group. This cabin is the perfect destination for nature lovers, explorers and every outdoor enthusiast!",
-                location: "Gatlinburg, TN, USA"
-            },
-            {
-                coverImage: "pictures/rocky-mountain-cabin.jpg",
-                price: 100,
-                description:
-                    "Ahhhhh.......Vacation time high in the Colorado Rocky Mountains. This Shadow Mountain Lake front home offers you that perfect “Getaway” you've always dreamed of. \
+            location: "Helen, GA, USA"
+        },
+        {
+            coverImage: "pictures/gatlinburg-cabin.jpg",
+            price: 150,
+            description:
+                "Get away from it all and spend some quality time in the Great Smoky Mountains! This 2-bedroom, 2.5-bathroom vacation rental cabin in Gatlinburg comfortably sleeps 6 guests who are looking for a quiet mountain holiday. The 2,500-square-foot home features a beautiful porch that looks out to the woods, as well as a private hot tub with room for everyone in your group. This cabin is the perfect destination for nature lovers, explorers and every outdoor enthusiast!",
+            location: "Gatlinburg, TN, USA"
+        },
+        {
+            coverImage: "pictures/rocky-mountain-cabin.jpg",
+            price: 100,
+            description:
+                "Ahhhhh.......Vacation time high in the Colorado Rocky Mountains. This Shadow Mountain Lake front home offers you that perfect “Getaway” you've always dreamed of. \
                     Whether you're looking for a quiet retreat in a pristine natural setting or that quality vacation fun \
                     time with family and friends, this could be your 'Slice of Heaven.'",
-                location: "Grand Lake, CO, USA"
-            },
-            {
-                coverImage: "pictures/michigan-cabin.jpg",
-                price: 150,
-                description:
-                    "This lovely cabin in a private setting is spacious yet has a warm rustic feeling. Built in the 1940s, it has a stone fireplace, maple hardwood floors, pine walls, a cathedral ceiling in the living room with a cozy sleeping loft on the mezzanine, plus two bedrooms on the main level with queen beds. There is a separate dining room that seats 6 and a breakfast room off of the kitchen. The wraparound porch has chairs and a large round table for dining or just sipping coffee.It is surrounded by a very large open lawn with mature trees and looks out at a riding stables. You can sit on the large wraparound porch or walk over to the stables and take a trail ride or riding lesson in the large indoor arena. Pony rides are also available for children. The beach is a short walk, or drive for 1 minute, park in our private spot, and walk around the corner to the beach.",
-                location: "Michiana Shores, IN, USA"
-            },
-            {
-                coverImage: "pictures/lake-tahoe-cabin.jpg",
-                price: 150,
-                description:
-                    "Treat yourself to the ultimate California retreat with this charming 2-bedroom, 1-bath South Lake Tahoe vacation rental cabin, which sleeps 6. Your vacation begins the moment you arrive at this classic 1950's California log cabin, nestled away amongst towering trees in South Lake Tahoe. Offering top-notch amenities and an unbeatable location minutes from Lake Tahoe, Heavenly Ski Resort and much more, this home is sure to provide an unforgettable experience!",
-                location: "Sierra Tract, South Lake Tahoe, CA, USA"
-            },
-            {
-                coverImage: "pictures/yellowstone-cabin.jpg",
-                price: 150,
-                description:
-                    "Yellowstone Cabin Rental On Private Acreage Five Miles From The Park's North Gate And Best Wildlife Watching And Cutthroat Trout Fishing! \
+            location: "Grand Lake, CO, USA"
+        },
+        {
+            coverImage: "pictures/michigan-cabin.jpg",
+            price: 120,
+            description:
+                "This lovely cabin in a private setting is spacious yet has a warm rustic feeling. Built in the 1940s, it has a stone fireplace, maple hardwood floors, pine walls, a cathedral ceiling in the living room with a cozy sleeping loft on the mezzanine, plus two bedrooms on the main level with queen beds. There is a separate dining room that seats 6 and a breakfast room off of the kitchen. The wraparound porch has chairs and a large round table for dining or just sipping coffee.It is surrounded by a very large open lawn with mature trees and looks out at a riding stables. You can sit on the large wraparound porch or walk over to the stables and take a trail ride or riding lesson in the large indoor arena. Pony rides are also available for children. The beach is a short walk, or drive for 1 minute, park in our private spot, and walk around the corner to the beach.",
+            location: "Michiana Shores, IN, USA"
+        },
+        {
+            coverImage: "pictures/lake-tahoe-cabin.jpg",
+            price: 130,
+            description:
+                "Treat yourself to the ultimate California retreat with this charming 2-bedroom, 1-bath South Lake Tahoe vacation rental cabin, which sleeps 6. Your vacation begins the moment you arrive at this classic 1950's California log cabin, nestled away amongst towering trees in South Lake Tahoe. Offering top-notch amenities and an unbeatable location minutes from Lake Tahoe, Heavenly Ski Resort and much more, this home is sure to provide an unforgettable experience!",
+            location: "Sierra Tract, South Lake Tahoe, CA, USA"
+        },
+        {
+            coverImage: "pictures/yellowstone-cabin.jpg",
+            price: 200,
+            description:
+                "Yellowstone Cabin Rental On Private Acreage Five Miles From The Park's North Gate And Best Wildlife Watching And Cutthroat Trout Fishing! \
                     Charming cabin built in 2009 with two bedrooms that sleeps 5-6 located just five minutes from Yellowstone Park's North Entrance on 26 acres of private property. Perfect for travelers who want easy access to the famous Northern Range and Lamar Valley with its bison, elk, deer, wolves, bears, AND fishing for world-famous Yellowstone cutthroat trout! \
                     This beautiful cabin is unlike all other vacation rental properties in the area. Lying in an extremely private location just above Highway 89 north of Gardiner, Montana, and the Roosevelt Arch, the cabin is easily accessed by a well-maintained, short, 300 yard-long Private Drive. \
                     This setting provides you with total solitude and isolation from any hustle and bustle of the busy roadways leading to/from the park. However, at the same time, when needed, you are just minutes away from restaurants, the market, and shops in Gardiner or a bit further away in Livingston and Bozeman. \
@@ -55,8 +54,7 @@ var RENTAL_DATA = {
                     Horses graze nearby, and the Yellowstone River, home to world-class trout fishing, leaves the park and passes just a few hundred yards below you. \
                     There are many access sites to the Yellowstone River very near to the cabin, one within easy walking distance (that is a very great fishing spot...just ask our boys!). \
                     Being near Gardiner, our cabin is at the home of the Yellowstone Association, a long-standing educational institute offering numerous courses in the park. The Electric Peak Cabin is a perfect place to stay when visiting and attending these courses.",
-                location: "Gardiner, MT, USA"
-            }
-        ]
-    }
+            location: "Gardiner, MT, USA"
+        }
+    ]
 };
