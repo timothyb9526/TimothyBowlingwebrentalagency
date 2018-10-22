9294a464d852b0612eabddbe1ac63b1e8b371936
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-10-22 16:15:36 -0500

    updated the forms

--- javascript.js
+++ javascript.js
@@ -71,7 +71,20 @@ function rentItem() {
         email +
         "<br />" +
         "Phone number: " +
-        phone +
+        "(" +
+        phone[0] +
+        phone[1] +
+        phone[2] +
+        ")" +
+        "-" +
+        phone[3] +
+        phone[4] +
+        phone[5] +
+        "-" +
+        phone[6] +
+        phone[7] +
+        phone[8] +
+        phone[9] +
         "<br />" +
         "Mailing adrress: " +
         address +
